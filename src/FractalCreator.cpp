b7da3e5b6dd6b048b406e63b174f54ccc1dcdf15
George Ford <george.ford@icloud.com>
2022-01-24 16:02:03 -0500

    updated FractalCretor

--- src/FractalCreator.cpp
+++ src/FractalCreator.cpp
@@ -7,3 +7,33 @@
 FractalCreator::FractalCreator(int width, int height)
 {
 }
+
+// DTOR
+/******************************************************************************/
+FractalCreator::~FractalCreator()
+{
+}
+
+// CALCULATE ITERATION
+/******************************************************************************/
+void FractalCreator::calculateIteration()
+{
+}
+
+// DRAW FRACTAL
+void FractalCreator::drawFractal()
+/******************************************************************************/
+{
+}
+
+// ADD ZOOM
+/******************************************************************************/
+void FractalCreator::addZoom(const Zoom& zoom)
+{
+}
+
+// WRITE BITMAP
+/******************************************************************************/
+void FractalCreator::writeBitmap(const std::string& name)
+{
+}
